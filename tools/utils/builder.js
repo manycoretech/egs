2811f15712983a2162d5c8a83be51be80b893cb6
daodao <daodao@qunhemail.com>
2026-07-22 11:02:21 +0800

    feat: simplify package.json before publish

--- tools/utils/builder.js
+++ tools/utils/builder.js
@@ -1,5 +1,5 @@
-import { existsSync, rmSync, mkdirSync, copyFileSync, readFileSync } from 'fs';
-import { relative, resolve, dirname } from 'path';
+import { existsSync, rmSync, mkdirSync, copyFileSync, readFileSync, writeFileSync } from 'node:fs';
+import { relative, resolve, dirname } from 'node:path';
 import chalk from 'chalk';
 import { sync } from 'glob';
 import { $ } from './process.js';
@@ -23,15 +23,23 @@ export function build(cp, release, typeOnly) {
         });
     }
     console.log(chalk.bold.green('[build]: copy file finished.'));
+    const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+    const typeOnlyExports = packageJson.typeOnlyExports;
+    const distPackageJsonPath = relative('.', resolve('./build', './package.json'));
+    delete packageJson.typeOnlyExports;
+    delete packageJson.devDependencies;
+    delete packageJson.scripts;
+    delete packageJson.release;
+    writeFileSync(distPackageJsonPath, JSON.stringify(packageJson, undefined, 2));
+    console.log(chalk.bold.green('[build]: package.json prepared.'));
 
     if (release) {
-        const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
         rollup(
             process.cwd(),
-            packageJson.typeOnlyExports
+            typeOnlyExports
                 ? {
-                      typeOnlyExports: packageJson.typeOnlyExports,
-                  }
+                    typeOnlyExports,
+                }
                 : undefined,
         );
     }
