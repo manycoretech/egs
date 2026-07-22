d669178a6f6d90cc97e4ef12a11f58e8a405fee5
daodao <daodao@qunhemail.com>
2026-07-22 14:54:09 +0800

    feat: move tslib into catalog

--- tools/utils/builder.js
+++ tools/utils/builder.js
@@ -5,10 +5,50 @@ import { sync } from 'glob';
 import { $ } from './process.js';
 import { rollup } from './dts-rollup.js';
 
+const workspaceRoot =
+    $('git rev-parse --show-superproject-working-tree', undefined, false).trimEnd('\n') ||
+    resolve(import.meta.dirname, '../../');
+
+const publishDependencyFields = ['dependencies', 'peerDependencies', 'optionalDependencies'];
+
+function resolveCatalogs() {
+    const data = {};
+    const result = {};
+    let r = $('pnpm config get catalogs --location=project --json', workspaceRoot, false);
+    if (r) {
+        Object.assign(data, JSON.parse(r));
+    } else {
+        r = $('pnpm config get catalog --location=project --json', workspaceRoot, false);
+        if (r) {
+            Object.assign(data, JSON.parse(r));
+        }
+    }
+
+    for (const key of Object.keys(data)) {
+        if (typeof data[key] === 'string') {
+            if (!result[key]) {
+                result[key] = {};
+            }
+            result[key]['default'] = data[key];
+        } else {
+            const versions = data[key];
+            for (const p of Object.keys(versions)) {
+                if (!result[p]) {
+                    result[p] = {};
+                }
+                result[p][`${key}`] = versions[p];
+            }
+        }
+    }
+    return result;
+}
+
 export function build(cp, release, typeOnly) {
     if (existsSync('./build')) {
         rmSync('./build', { recursive: true });
     }
+
+    const catalogs = resolveCatalogs();
 
     $(`pnpm tsc -b ${typeOnly ? '--emitDeclarationOnly' : ''}`);
 
@@ -29,6 +69,20 @@ export function build(cp, release, typeOnly) {
     delete packageJson.devDependencies;
     delete packageJson.scripts;
     delete packageJson.release;
+
+    // resolve catalog
+    for (const field of publishDependencyFields) {
+        const dependencies = packageJson[field];
+        if (dependencies) {
+            for (const p of Object.keys(dependencies)) {
+                if (dependencies[p].startsWith('catalog:')) {
+                    const version = dependencies[p].slice('catalog:'.length) || 'default';
+                    dependencies[p] = catalogs[p][version];
+                }
+            }
+        }
+    }
+
     writeFileSync(relative('.', resolve('./build', './package.json')), JSON.stringify(packageJson, undefined, 2));
     console.log(chalk.bold.green('[build]: package.json for publish written.'));
 
